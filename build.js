43968884585c85455f1738e685fc316678a168c2
Charlie Owen <charlie@sonniesedge.co.uk>
2018-02-23 20:39:36 +0100

    Basic templating

--- build.js
+++ build.js
@@ -1,7 +1,16 @@
 var Metalsmith = require('metalsmith'),
-    markdown   = require('metalsmith-markdown');
+    markdown   = require('metalsmith-markdown'),
+    templates  = require('metalsmith-templates'),
+    rename  = require('metalsmith-rename');
 
 Metalsmith(__dirname)
+    .source('./site/content')
+    .use(
+        rename([
+          [/\_index.md$/, "index.html"]
+        ])
+      )
     .use(markdown())
-    .destination('./build')
+    .use(templates('handlebars'))
+    .destination('./dist')
     .build(function (err) { if(err) console.log(err) });
