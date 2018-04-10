b13cde3efd5e34d4fcaf29c9ab676ca9f280235f
Charlie Owen <charlie@sonniesedge.co.uk>
2018-04-10 14:36:40 +0200

    Breakdown the webmentions figure

--- metalsmith-webmentions.js
+++ metalsmith-webmentions.js
@@ -15,7 +15,7 @@ function plugin(options) {
                 setImmediate(done); // effectively feature flag this
                 // throw new Error('Webmentions.io key is not defined in env vars!');
             }  else {
-                let apiUrl = `https://webmention.io/api/mentions?domain=sonniesedge.co.uk&token=${process.env.WEBMENTTOKEN}&per-page=2000`;
+                let apiUrl = `https://webmention.io/api/mentions?domain=sonniesedge.co.uk&token=${process.env.WEBMENTTOKEN}&per-page=20000`;
         
                 request.get({url: apiUrl, json: true}, (err, response) => {
                     let apiData = response.body;
@@ -27,6 +27,10 @@ function plugin(options) {
 
                         let webmentionCount = 0;
 
+                        let webmentionCountReply = 0;
+                        let webmentionCountLike = 0;
+                        let webmentionCountRepost = 0;
+
                         // Loop through each value in api object
                         for (var i = 0, len = apiData['links'].length; i < len; i++) {
                             // if api URL matches the current file URL then...
@@ -34,10 +38,28 @@ function plugin(options) {
                                 // console.log(metalsmith.metadata().site.url + files[file]['path'] + ' got a ' + apiData['links'][i]['activity']['type']);
                                 // console.log('API: ', apiData['links'][i]['target']);
                                 // console.log('MS:  ', metalsmith.metadata().site.url + files[file]['path']);
+                                console.log(apiData['links'][i]['activity']['type']);
                                 webmentionCount ++;
+
+                                if (apiData['links'][i]['activity']['type'] === 'reply') {
+                                    webmentionCountReply ++;
+                                }
+
+                                if (apiData['links'][i]['activity']['type'] === 'like') {
+                                    webmentionCountLike ++;
+                                }
+
+                                if (apiData['links'][i]['activity']['type'] === 'repost') {
+                                    webmentionCountRepost ++;
+                                }
+
+
                             }
                         }
                         files[file]['webmentionCount'] = webmentionCount;
+                        files[file]['webmentionCountLike'] = webmentionCountLike;
+                        files[file]['webmentionCountReply'] = webmentionCountReply;
+                        files[file]['webmentionCountRepost'] = webmentionCountRepost;
                     });
 
                     setImmediate(done);
