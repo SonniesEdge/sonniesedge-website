1526ffa52a0ed86fd47a04bca74a26d3b31c5b09
Charlie Owen <charlie@sonniesedge.co.uk>
2018-04-12 18:01:35 +0200

    Add some structure to add webmention data to

--- metalsmith-webmentions.js
+++ metalsmith-webmentions.js
@@ -3,26 +3,28 @@ var type = require('component-type');
 
 module.exports = plugin;
 
-// console.log(process.env.WEBMENTTOKEN); 
+console.log(process.env.WEBMENTTOKEN);
 
 function plugin(options) {
 
     options = options || {};
 
-    return function(files, metalsmith, done){
+    return function (files, metalsmith, done) {
         try {
             if (!process.env.WEBMENTTOKEN) {
                 setImmediate(done); // effectively feature flag this
                 // throw new Error('Webmentions.io key is not defined in env vars!');
-            }  else {
-                let apiUrl = `https://webmention.io/api/mentions?domain=sonniesedge.co.uk&token=${process.env.WEBMENTTOKEN}&per-page=20000`;
-        
-                request.get({url: apiUrl, json: true}, (err, response) => {
+            } else {
+                let apiUrl = `https://webmention.io/api/mentions?domain=sonniesedge.co.uk&token=${process.env.WEBMENTTOKEN}&per-page=2000`;
+
+                console.log(apiUrl);
+
+                request.get({ url: apiUrl, json: true }, (err, response) => {
                     let apiData = response.body;
-           
-        
+
+
                     // Loop through each file
-                    Object.keys(files).forEach(function(file){
+                    Object.keys(files).forEach(function (file) {
                         var data = files[file];
 
                         let webmentionCount = 0;
@@ -31,6 +33,10 @@ function plugin(options) {
                         let webmentionCountLike = 0;
                         let webmentionCountRepost = 0;
 
+                        let webmentionReplies = [];
+                        let webmentionLikes = [];
+                        let webmentionReposts = [];
+
                         // Loop through each value in api object
                         for (var i = 0, len = apiData['links'].length; i < len; i++) {
                             // if api URL matches the current file URL then...
@@ -38,19 +44,34 @@ function plugin(options) {
                                 // console.log(metalsmith.metadata().site.url + files[file]['path'] + ' got a ' + apiData['links'][i]['activity']['type']);
                                 // console.log('API: ', apiData['links'][i]['target']);
                                 // console.log('MS:  ', metalsmith.metadata().site.url + files[file]['path']);
-                                console.log(apiData['links'][i]['activity']['type']);
-                                webmentionCount ++;
+                                // console.log('TYPE: ', apiData['links'][i]['activity']['type']);
+                                webmentionCount++;
 
                                 if (apiData['links'][i]['activity']['type'] === 'reply') {
-                                    webmentionCountReply ++;
+                                    webmentionCountReply++;
+                                    webmentionReplies.push(
+                                        {
+                                            
+                                        }
+                                    );
                                 }
 
                                 if (apiData['links'][i]['activity']['type'] === 'like') {
-                                    webmentionCountLike ++;
+                                    webmentionCountLike++;
+                                    webmentionLikes.push(
+                                        {
+
+                                        }
+                                    );
                                 }
 
                                 if (apiData['links'][i]['activity']['type'] === 'repost') {
-                                    webmentionCountRepost ++;
+                                    webmentionCountRepost++;
+                                    webmentionReposts.push(
+                                        {
+
+                                        }
+                                    );
                                 }
 
 
@@ -64,10 +85,10 @@ function plugin(options) {
 
                     setImmediate(done);
                 });
-                
+
             }
         }
-        catch(err) {
+        catch (err) {
             console.log(err);
         }
     }
