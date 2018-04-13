084dd5e2c0cd298b72e5c28f063b9c19f86a7934
Charlie Owen <charlie@sonniesedge.co.uk>
2018-04-13 14:44:10 +0200

    Add more details for likes, mentions, and reposts

--- metalsmith-webmentions.js
+++ metalsmith-webmentions.js
@@ -1,5 +1,7 @@
 var request = require('request');
 var type = require('component-type');
+var _ = require('lodash');
+var moment = require('moment');
 
 module.exports = plugin;
 
@@ -29,11 +31,6 @@ function plugin(options) {
 
                         files[file]['webmentionCount'] = 0;
 
-                        files[file]['webmentionCountReply'] = 0;
-                        files[file]['webmentionCountLike'] = 0;
-                        files[file]['webmentionCountRepost'] = 0;
-                        files[file]['webmentionCountLink'] = 0;
-
                         files[file]['webmentionReplies'] = [];
                         files[file]['webmentionLikes'] = [];
                         files[file]['webmentionReposts'] = [];
@@ -49,40 +46,43 @@ function plugin(options) {
                                 // console.log('TYPE: ', apiData['links'][i]['activity']['type']);
                                 files[file]['webmentionCount']++;
 
-                                if (apiData['links'][i]['activity']['type'] === 'reply') {
-                                    files[file]['webmentionCountReply']++;
-                                    files[file]['webmentionReplies'].push(
-                                        {
-
-                                        }
-                                    );
-                                }
 
                                 if (apiData['links'][i]['activity']['type'] === 'like') {
                                     files[file]['webmentionCountLike']++;
-                                    files[file]['webmentionLikes'].push(
-                                        {
+                                    let payload = {};
 
-                                        }
-                                    );
+                                    payload.author_name = _.get(apiData['links'][i], 'data.author.name', 'Unknown');
+                                    payload.author_url = _.get(apiData['links'][i], 'data.author.url', 'Unknown');
+                                    payload.id = _.get(apiData['links'][i], 'id', 0);
+
+                                    files[file]['webmentionLikes'].push(payload);
                                 }
 
                                 if (apiData['links'][i]['activity']['type'] === 'repost') {
                                     files[file]['webmentionCountRepost']++;
-                                    files[file]['webmentionReposts'].push(
-                                        {
+                                    let payload = {};
 
-                                        }
-                                    );
+                                    payload.author_name = _.get(apiData['links'][i], 'data.author.name', 'Unknown');
+                                    payload.author_url = _.get(apiData['links'][i], 'data.author.url', 'Unknown');
+                                    payload.repost_source = _.get(apiData['links'][i], 'data.url', 'Unknown');
+                                    payload.date = moment(_.get(apiData['links'][i], 'data.published', '1970-01-01')).format("MMMM Do YYYY");
+                                    payload.id = _.get(apiData['links'][i], 'id', 0);
+
+                                    files[file]['webmentionReposts'].push(payload);
                                 }
 
                                 if (apiData['links'][i]['activity']['type'] === 'link') {
                                     files[file]['webmentionCountLink']++;
-                                    files[file]['webmentionLinks'].push(
-                                        {
+                                    let payload = {};
 
-                                        }
-                                    );
+                                    payload.author_name = _.get(apiData['links'][i], 'data.author.name', 'Unknown');
+                                    payload.author_url = _.get(apiData['links'][i], 'data.author.url', 'Unknown');
+                                    payload.date = moment(_.get(apiData['links'][i], 'data.published', '1970-01-01')).format("MMMM Do YYYY");
+                                    payload.link_source = _.get(apiData['links'][i], 'data.url', 'Unknown');
+                                    payload.content = _.get(apiData['links'][i], 'data.content', 'Unknown');
+                                    payload.id = _.get(apiData['links'][i], 'id', 0);
+
+                                    files[file]['webmentionLinks'].push(payload);
                                 }
 
 
