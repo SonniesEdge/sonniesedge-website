fca2aa40f4554ba78e2d9bcfdb4d5ec073163f2b
Charlie Owen <charlie@sonniesedge.co.uk>
2018-09-13 20:18:24 +0200

    Download zip file

--- metalsmith-dropbox.js
+++ metalsmith-dropbox.js
@@ -3,6 +3,8 @@ var slugify = require('slugify');
 var marked = require('marked');
 var type = require('component-type');
 var querystring = require('query-string');
+var fetch = require('isomorphic-fetch');
+var fs = require('fs');
 
 module.exports = plugin;
 
@@ -18,16 +20,19 @@ function plugin(options) {
                 setImmediate(done); // effectively feature flag this
                 // throw new Error('Webmentions.io key is not defined in env vars!');
             } else {
-                var fetch = require('isomorphic-fetch'); // or another library of choice.
+                 // or another library of choice.
                 var Dropbox = require('dropbox').Dropbox;
                 var dbx = new Dropbox({ accessToken: process.env.DROPBOXTOKEN, fetch: fetch });
-                dbx.filesListFolder({path: '/test'})
-                .then(function(response) {
-                    console.log(response);
-                })
-                .catch(function(error) {
-                    console.log(error);
-                });
+                dbx.filesDownloadZip({path: '/test'})
+                    .then(function(response) {
+                        console.log(response);
+                        fs.writeFile('test.zip', response.fileBinary, 'binary', (err) => {
+                            console.log('done');
+                        });
+                    })
+                    .catch(function(error) {
+                        console.log(error);
+                    });
 
                 setImmediate(done);
             }
