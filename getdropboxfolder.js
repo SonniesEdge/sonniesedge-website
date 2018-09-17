fd9124c9a8e4cd2f3564d2a4d1f907fb7cf2721f
Charlie Owen <charlie@sonniesedge.co.uk>
2018-09-17 09:11:05 +0200

    Filesystem copy on local dev machine

--- getdropboxfolder.js
+++ getdropboxfolder.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import unzip from 'adm-zip';
 import mv from  'mv';
 import path from 'path';
+import copy from 'copy';
 
 var tmpZipFile = 'tmpdropboxdata.zip';
 var tmpExtractionDir = './tmp';
@@ -12,23 +13,43 @@ function plugin(options, callback) {
         if (!process.env.DROPBOXTOKEN) {
             throw new Error('No Dropbox token specified.');
         } else {
-            var Dropbox = require('dropbox').Dropbox;
-            var dbx = new Dropbox({ accessToken: process.env.DROPBOXTOKEN, fetch: fetch });
-            dbx.filesDownloadZip({path: options.dropboxfolder})
-            .then((result) => {
-                fs.writeFile(tmpZipFile, result.fileBinary, 'binary', (err) => {
-                    let zip = new unzip(tmpZipFile);
 
-                    var zipEntries = zip.getEntries();
-                    zip.extractAllTo(tmpExtractionDir);
+            var env = process.env.NODE_ENV || 'dev';
 
-                    let tmpExtractionSubDir = fs.readdirSync(tmpExtractionDir)[0];
+            if (env === 'dev') {
+                // Get location of dropbox file
+                let dropboxPath = '/Users/charlie/Dropbox';
 
-                    mv(path.join(tmpExtractionDir, tmpExtractionSubDir), options.localDestination, {mkdirp: true}, function(err) {
-                        callback();
+                // Combine paths
+                let dropboxFullPath = path.join(dropboxPath, options.dropboxfolder);
+
+                // Copy files on local filesystem
+                copy(dropboxFullPath + '/**', options.localDestination, function(err, files) {
+                    if (err) throw err;
+                    callback();
+                });
+               
+            } else {
+                var Dropbox = require('dropbox').Dropbox;
+                var dbx = new Dropbox({ accessToken: process.env.DROPBOXTOKEN, fetch: fetch });
+                dbx.filesDownloadZip({path: options.dropboxfolder})
+                .then((result) => {
+                    fs.writeFile(tmpZipFile, result.fileBinary, 'binary', (err) => {
+                        let zip = new unzip(tmpZipFile);
+    
+                        var zipEntries = zip.getEntries();
+                        zip.extractAllTo(tmpExtractionDir);
+    
+                        let tmpExtractionSubDir = fs.readdirSync(tmpExtractionDir)[0];
+    
+                        mv(path.join(tmpExtractionDir, tmpExtractionSubDir), options.localDestination, {mkdirp: true}, function(err) {
+                            callback();
+                        });
                     });
                 });
-            })
+            }
+
+
         }
     }
     catch (err) {
