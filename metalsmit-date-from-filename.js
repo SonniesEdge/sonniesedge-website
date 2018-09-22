c68fc277db05280335e57465a52c52579be1a4b7
Charlie Owen <charlie@sonniesedge.co.uk>
2018-09-22 17:17:05 +0200

    Get working and make more compact

--- metalsmit-date-from-filename.js
+++ metalsmit-date-from-filename.js
@@ -1,40 +1,20 @@
-var _ = require('lodash');
-var moment = require('moment-timezone');
-moment.tz.setDefault("Europe/Berlin");
+var moment = require('moment');
 
 module.exports = function (options) {
   var override = (('boolean' === typeof options) && options) || ('object' === typeof options && options.override) || false;
   
   return function drafts(files, metalsmith, done) {
-    _.forEach(files, function (fileMeta, fileName) {
+    Object.entries(files).forEach(([fileName, fileMeta]) => {
       if (!override && fileMeta.date) {
         return;
       }
-      var m;
-      if (m = fileName.match(/(\d{4}-\d{2}-\d{2}t\d{2}-\d{2})/)) {
-        console.log('DATE: ', m[1]);
-
-        let date = m[1];
-
-        var momentDate = moment(date, 'YYYY-MM-DD[t]hh:mm');
-        var jsDate = momentDate.toDate();
-
-        console.log('Moment date: ', momentDate);
-        console.log('JS date: ', jsDate);
-
-        fileMeta.date = new Date(jsDate);
+      let date = fileName.match(/(\d{4}-\d{2}-\d{2}t\d{2}-\d{2})/)
+      if (date) {
+        let momentDate = moment(date[1], 'YYYY-MM-DD[t]HH:mm').format('YYYY-MM-DD HH:mm');
+        fileMeta.date = momentDate;
       }
     });
-
-    var filesWithoutContents = _.zipObject(_.map(files, function (fileMeta, fileName) {
-      var filteredFileMeta = _.reduce(fileMeta, function (acc, metaValue, metaName) {
-        if ('contents' !== metaName) {
-          acc[metaName] = metaValue;
-        }
-        return acc;
-      }, {});
-      return [fileName, filteredFileMeta];
-    }));
+    
     done();
   };
 };
